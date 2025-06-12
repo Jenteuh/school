9452bf40580eb3b1a46e782dcf1b2259d62e44cb
Jente Huysmans <jentehuysmans@hotmail.com>
2025-06-12 15:54:04 +0200

    application.properties

---
+++ src/main/java/be/vdab/school/leerlingen/CsvLeerlingRepository.java
@@ -0,0 +1,34 @@
+package be.vdab.school.leerlingen;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+@Component
+public class CsvLeerlingRepository implements LeerlingRepository {
+
+    private final String pad;
+
+    public CsvLeerlingRepository(@Value("${leerlingenCsvPad}") String pad) {
+        this.pad = pad;
+    }
+
+    @Override
+    public List<Leerling> findAll() {
+        try (var regels = Files.lines(Path.of(pad))) {
+            return regels
+                    .map(regel -> regel.split(",")) // regel splitsen in zijn onderdelen
+                    .map(regelOnderdelen -> // Leerling object maken met die onderdelen
+                            new Leerling(Long.parseLong(regelOnderdelen[0]), regelOnderdelen[1], regelOnderdelen[2]))
+                    .toList();
+        } catch (IOException | ArrayIndexOutOfBoundsException |
+                 NumberFormatException ex) {
+            throw new IllegalArgumentException("Leerlingenbestand bevat verkeerde data", ex);
+        }
+    }
+
+}
--- src/main/java/be/vdab/school/leerlingen/LeerlingRepository.java
+++ src/main/java/be/vdab/school/leerlingen/LeerlingRepository.java
@@ -1,26 +1,7 @@
 package be.vdab.school.leerlingen;
 
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
-@Component
-public class LeerlingRepository {
-
-    public List<Leerling> findAll() {
-        try (var regels = Files.lines(Path.of("/data/leerlingen.csv"))) {
-            return regels
-                    .map(regel -> regel.split(",")) // regel splitsen in zijn onderdelen
-                    .map(regelOnderdelen -> // Leerling object maken met die onderdelen
-                            new Leerling(Long.parseLong(regelOnderdelen[0]), regelOnderdelen[1], regelOnderdelen[2]))
-                    .toList();
-        } catch (IOException | ArrayIndexOutOfBoundsException |
-                 NumberFormatException ex) {
-            throw new IllegalArgumentException("Leerlingenbestand bevat verkeerde data", ex);
-        }
-    }
-
+public interface LeerlingRepository {
+    List<Leerling> findAll();
 }
